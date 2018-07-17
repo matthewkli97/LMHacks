c65d9f740159bf14cc407f5e6abd1519dfb44697
Matthew Li <matthewli@MacBookPro.local>
2018-07-16 21:27:42 -0700

    image upload icon

--- app/src/main/java/com/seattle/hack/lmhack/MainActivity.kt
+++ app/src/main/java/com/seattle/hack/lmhack/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : Activity() {
 
+    private var mChats: ArrayList<Message>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
